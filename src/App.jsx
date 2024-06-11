3e8b88911deadd97768c1ffe0c6e8be3b7bca2ee
EduarMendozaL <161656434+EduarMendozaL@users.noreply.github.com>
2024-06-11 10:19:05 -0400

    Proyecto del curso por terminar

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components"
+import GlobalStyle from "./components/GlobalStyles"
+import Cabecera from "./components/Cabecera"
+import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
+import banner from "./assets/banner.png"
+import Galeria from "./components/Galeria"
+import fotos from "./fotos.json"
+import { useState } from "react"
+import ModalZoom from "./components/ModalZoom"
+
+const FondoGradiente = styled.div`
+background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+width: 100%;
+min-height: 100vh;
+`
+
+const AppContainer = styled.div`
+  width: 1280px;
+  max-width: 100%;
+  margin: 0 auto;
+`
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+const ContenidoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+const App = () => {
+  const [fotosDeGaleria, setFotosDeGaleria] = useState(fotos)
+  const [fotoSeleccionada, setFotoSeleccionada] = useState(null)
+
+  // ({foto})->se pasa como un objeto
+  // (foto)->sin los corchetes, se pasa como un parámetro
+  const alAlternarFavorito = (foto) => {
+
+    if (foto.id === fotoSeleccionada?.id) {
+      setFotoSeleccionada({
+        ...fotoSeleccionada,
+        favorita: !foto.favorita
+      })
+    }
+
+    setFotosDeGaleria(fotosDeGaleria.map(fotoDeGaleria => {
+      return {
+        ...fotoDeGaleria,
+        favorita: fotoDeGaleria.id === foto.id ? !foto.favorita : fotoDeGaleria.favorita
+      }
+    }))
+  }
+
+  return (
+    <>
+      <FondoGradiente>
+        <GlobalStyle />
+        <AppContainer>
+          <Cabecera />
+          <MainContainer>
+            <BarraLateral />
+            <ContenidoGaleria>
+              <Banner texto="La galería más completa del espacio" backgroundImage={banner} />
+              <Galeria alSeleccionarFoto={foto => setFotoSeleccionada(foto)} fotos={fotosDeGaleria} alAlternarFavorito={alAlternarFavorito} />
+            </ContenidoGaleria>
+          </MainContainer>
+        </AppContainer>
+        <ModalZoom foto={fotoSeleccionada} close={() => setFotoSeleccionada(null)} alAlternarFavorito={alAlternarFavorito} />
+      </FondoGradiente>
+    </>
+  )
+}
+
+export default App
